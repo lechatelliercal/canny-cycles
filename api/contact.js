0c8cc79d5396e4b1c1a0e107755f51fdad994787
Callum Lechatellier <Callum.lechat@gmail.com>
2026-06-08 11:10:01 +0100

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM = 'Canny Cycle Services <callum@send.cannycycles.com>';
+const FROM = 'Canny Cycle Services <callum@cannycycles.com>';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
